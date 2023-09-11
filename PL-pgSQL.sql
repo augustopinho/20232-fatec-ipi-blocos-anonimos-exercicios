047610cd73ea08565eed26c9d024148c13c1dec0
augustopinho <117299034+augustopinho@users.noreply.github.com>
2023-09-11 20:55:02 -0300

    build (criando o repositorio): Primeiro commit

---
+++ PL-pgSQL.sql
@@ -0,0 +1,8 @@
+-- 1.1 Faça um programa que gere um valor inteiro e o exiba.
+DO $$
+DECLARE
+	n1 NUMERIC (5, 2);
+BEGIN
+	n1 := floor (random () * 100 + 1) :: INT;
+	RAISE NOTICE 'n1: %', n1;
+END $$;
