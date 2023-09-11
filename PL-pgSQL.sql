4ced4a6de25412d312c196e71127fbcd974444b8
augustopinho <117299034+augustopinho@users.noreply.github.com>
2023-09-11 20:57:47 -0300

    feat: adicionando exercício 2

--- PL-pgSQL.sql
+++ PL-pgSQL.sql
@@ -6,3 +6,12 @@ BEGIN
 	n1 := floor (random () * 100 + 1) :: INT;
 	RAISE NOTICE 'n1: %', n1;
 END $$;
+
+--1.2. Faça um programa que gere um valor real e o exiba.
+DO $$
+DECLARE
+	n1 NUMERIC (5, 2);
+BEGIN
+	n1 := random () * 9 + 1;
+	RAISE NOTICE 'n1: %', n1;
+END $$;
