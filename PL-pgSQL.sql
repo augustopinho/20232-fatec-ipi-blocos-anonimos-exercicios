2d0865ea7b518baf4b7432a90e38e667110e6504
augustopinho <117299034+augustopinho@users.noreply.github.com>
2023-09-11 20:58:33 -0300

    feat: adicionando exercício 3

--- PL-pgSQL.sql
+++ PL-pgSQL.sql
@@ -15,3 +15,14 @@ BEGIN
 	n1 := random () * 9 + 1;
 	RAISE NOTICE 'n1: %', n1;
 END $$;
+
+--1.3 Faça um programa que gere um valor real no intervalo [20, 30] que representa uma
+--temperatura em graus Celsius. Faça a conversão para Fahrenheit e exiba.
+DO $$
+DECLARE
+	n1 NUMERIC (5, 2);
+	n2 NUMERIC (5, 2);
+BEGIN
+	n1 := floor(random () * 11 + 20) :: INT;
+	RAISE NOTICE 'Temperatura em C°: % , Temperatura em F°: %', n1, n1 * 1.8 + 32;
+END $$
